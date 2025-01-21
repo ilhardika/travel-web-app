933268715ebfa7a5426b36de2e5ff8e85034590e
ilhardika <ilhardika67@gmail.com>
2025-01-21 14:26:50 +0700

    structure folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+// src/App.jsx
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
-    <div className="App">
-      <AppRoutes />
-    </div>
+    <AuthProvider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
 export default App;
+
+// Semua service API
+// Axios configuration
+// Interceptor
